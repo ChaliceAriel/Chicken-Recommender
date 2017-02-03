38edf73e4022b351a030e8cebb94fe00bfd69e51
Chalice Stevens <chaliceariel@gmail.com>
2017-02-03 14:17:49 -0700

    initial commit

---
+++ ChickenShortList.js
@@ -0,0 +1,123 @@
+var chickenList = [
+
+{
+name: "Amber White",
+image: "http://s7d9.scene7.com/is/image/murdochs/AmberWhite?$jpg$",
+text: "Created as a hybrid breed for the purpose of egg production.",
+bullets: ["Cream colored bird with brown speckles", "Great egg layer", "Cross breed between Rhode Island White & Rhode Island Red", "Egg Color: Brown"],
+attributes:
+    {
+    goodLayer: true,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: false,
+    broody: false,
+    docile: true
+    }
+},
+{
+name: "Araucana/Ameraucanas Mixed Breed",
+image: "http://s7d9.scene7.com/is/image/murdochs/Ameraucana2?$jpg$",
+text: 'Nicknamed "the easter egg chicken" for its ability to lay colored eggs, varying from turquoise to deep olive and shades of brown. It is a cross between the Araucana/Ameraucana cross, and therefore have different color variations as adults.',
+bullets: ["This breed derives from the Araucana but is a mix of breeds, which makes it not a show bird", "Famous for their blue and green eggs", ' Known as the "Easter Egg Bird" in multi-colored mixed white, brown, red and black feathers'],
+attributes:
+    {
+    goodLayer: true,
+    brownEggs: false,
+    whiteEggs: false,
+    multiEggs: true,
+    meatBird: false,
+    coldHardy: true,
+    broody: false,
+    docile: true
+    }
+},
+{
+name: "Barred Cochin Bantam",
+image: "http://s7d9.scene7.com/is/image/murdochs/Barred%2DCochin%2DBantam%2D1?$jpg$",
+text: "This breed is one of the all-time favorites. Their solid plumpness and yellow skin make a beautiful roasting fowl. Hardy, reliable chickens with bred-in strength and vitality.",
+bullets: ["Defined parallel bars of dark and light colors", "Very gentle and requires little space", "Egg Color: Brown"],
+attributes:
+    {
+    goodLayer: false,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: false,
+    broody: true,
+    docile: true
+    }
+},
+{
+name: "Barred Plymouth Rock",
+image: "http://s7d9.scene7.com/is/image/murdochs/Barred%2DRock%2DStRun?$jpg$",
+text: "Prolific layers of brown eggs and the hens are not discouraged by cold weather. Have a solid plumpness and yellow skin that make them a good roasting bird.",
+bullets: ["Prolific layers of brown eggs", "Egg Color: Brown"],
+attributes:
+    {
+    goodLayer: true,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: true,
+    broody: false,
+    docile: true
+    }
+},
+{
+name: "Black Australorp",
+image: "http://s7d9.scene7.com/is/image/murdochs/Black%2DAustralorp%2DStrRun?$jpg$",
+text: "Considered one of the best layers of light brown eggs of all heavy breeds. Glossy black with a greenish to purplish sheen. Females mature early and many will be in production between 5 & 6 months of age.",
+bullets: ["Layers of light brown eggs", "Black plumage with greenish to purplish sheen", "Females mature early", " Heavy breed"],
+attributes:
+    {
+    goodLayer: true,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: true,
+    broody: false,
+    docile: true
+    }
+},
+{
+name: "Black Cochin",
+image: "http://s7d9.scene7.com/is/image/murdochs/Black%2DCochin?$jpg$",
+text: "Have a fluffy, downy plumage giving them a somewhat massive, bulky appearance. Beautiful coal-black color. Highly prized for exhibition.",
+bullets: ["Sweet personalities and excellent mothering qualities", "Egg Color: Brown"],
+attributes:
+    {
+    goodLayer: false,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: true,
+    broody: true,
+    docile: true
+    }
+},
+{
+name: "Black Cochin Bantam",
+image: "http://s7d9.scene7.com/is/image/murdochs/Black%2DCochin%2DBantam?$jpg$",
+text: "Cochins are heavily feathered down the shanks and toes and appear to be much larger than they actually are. They are very gentle, excellent setters and require little space.",
+bullets: ["Coal-black plumage with a greenish sheen", "Bright red comb and wattle", "Small, very gentle birds", "Egg Color: Brown"],
+attributes:
+    {
+    goodLayer: false,
+    brownEggs: true,
+    whiteEggs: false,
+    multiEggs: false,
+    meatBird: false,
+    coldHardy: false,
+    broody: true,
+    docile: true
+    }
+} 
+
+]
